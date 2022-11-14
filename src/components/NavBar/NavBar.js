ae0d6a87cfee09b675c0ec464287ae5d57912b73
jun2212 <hsj890509@gmail.com>
2022-11-15 05:05:07 +0900

    feat: navbar 주소별 버튼 구현

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+
+import { COLOR } from "../../config/constants";
 
 function NavBar() {
+  const { pathname } = useLocation();
+
   return (
     <Wrapper>
       <ButtonWrapper>
-        <Button>로그인</Button>
+        {pathname === "/login" ? (
+          <Button>REGISTER</Button>
+        ) : pathname === "/register" ? (
+          <Button>LOGIN</Button>
+        ) : pathname === "/" ? (
+          <>
+            <Button>GUIDE</Button>
+            <Button>LOGOUT</Button>
+          </>
+        ) : pathname === "/guide" ? (
+          <>
+            <Button>MAIN</Button>
+            <Button>LOGOUT</Button>
+          </>
+        ) : (
+          <Button>MAIN</Button>
+        )}
       </ButtonWrapper>
     </Wrapper>
   );
@@ -18,28 +38,29 @@ const Wrapper = styled.div`
   align-items: center;
   height: 4rem;
   border: none;
-  background: black;
+  background: ${COLOR.BLACK};
 `;
 
 const ButtonWrapper = styled.div`
   display: flex;
-  margin-right: 5rem;
+  margin-right: 3rem;
 `;
 
 const Button = styled.button`
-  margin-left: 5rem;
+  margin-left: 2rem;
   padding: 0.3rem;
   border: none;
   border-radius: 0.5rem;
-  background: white;
-  color: black;
+  width: 6rem;
+  background: ${COLOR.WHITE};
+  color: ${COLOR.BLACK};
   font-size: 1.2rem;
   font-weight: bolder;
   cursor: pointer;
 
   &:hover {
     border-bottom: 2px solid black;
-    color: black;
+    color: ${COLOR.BLUE};
   }
 `;
 
