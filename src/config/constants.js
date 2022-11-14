c5e40404d16bc60f00d75a8ec2f28b6de4c52565
jun2212 <hsj890509@gmail.com>
2022-11-15 05:05:55 +0900

    feat: constants 구현

---
+++ src/config/constants.js
@@ -0,0 +1,11 @@
+export const COLOR = {
+  BLACK: "#212426",
+  WHITE: "#FFFFFF",
+  GRAY: "#D9D9D9",
+  RED: "#CD264B",
+  YELLOW: "#FFCF00",
+  GREEN: "#007E49",
+  BLUE: "#0073B7",
+  DEEP_BLUE: "#293744",
+  PURPLE: "#471C8A",
+};
