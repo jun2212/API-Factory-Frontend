d209edb0acac6231e099d01e3f5e458493bb332c
jun2212 <hsj890509@gmail.com>
2022-11-15 05:04:32 +0900

    feat: 로그인 레이아웃 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+
+import { LoginForm } from "../../components/LoginForm/LoginForm";
 
 function Login() {
-  return <div>로그인</div>;
+  return (
+    <>
+      <ContentsWrapper>
+        <LoginForm />
+      </ContentsWrapper>
+    </>
+  );
 }
+
+const ContentsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 90vh;
+`;
 
 export { Login };
