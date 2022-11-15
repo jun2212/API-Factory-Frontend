7998b72cb59daf653529fbf6d78a2e032366740e
jun2212 <hsj890509@gmail.com>
2022-11-15 15:12:00 +0900

    feat: 사용자 코드입력 구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 
 import { COLOR } from "../../config/constants";
 
 import { CodeEditor } from "../../components/CodeEditor/CodeEditor";
+import { Modal } from "../../components/Modal/Modal";
 
 function Main() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [message, setMessage] = useState("");
+  const editorRef = useRef(null);
+  const validateRef = useRef(null);
+
+  const submitCode = () => {
+    setMessage(validationCode());
+    setIsModalOpen(true);
+  };
+  const validationCode = () => {
+    if (validateRef.current !== null && validateRef.current.length !== 0) {
+      return "함수의 형태가 잘못되었습니다.";
+    }
+    return "전송완료";
+  };
   return (
-    <ContentsWrapper>
-      <Tip>자세한 사용법은 GUIDE를 참고해 주세요.</Tip>
-      <CodeEditor />
-      <Button>함수 저장</Button>
-    </ContentsWrapper>
+    <>
+      {isModalOpen && (
+        <Modal
+          closeModal={() => setIsModalOpen(false)}
+          title={"titleMessage"}
+          message={message}
+        />
+      )}
+      <ContentsWrapper>
+        <Tip>자세한 사용법은 GUIDE를 참고해 주세요.</Tip>
+        <CodeEditor editorRef={editorRef} validateRef={validateRef} />
+        <Button onClick={submitCode}>함수 저장</Button>
+      </ContentsWrapper>
+    </>
   );
 }
 
@@ -43,9 +68,12 @@ const Button = styled.button`
   color: ${COLOR.WHITE};
   font-size: 1.3rem;
   font-weight: bolder;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+  cursor: pointer;
 
   &:hover {
-    background-color: ${COLOR.BLACK};
+    background-color: ${COLOR.WHITE};
     color: ${COLOR.RED};
     transition: all 200ms;
   }
